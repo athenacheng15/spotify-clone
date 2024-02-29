a06744c79c1df6245b18e570f99b0b9dbe6287c6
yuchuncheng <yuchuncheng@nine-yi.com>
2024-02-29 15:54:13 +0800

    feat : Player component structure and apply to layout

---
+++ components/player.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import useGetSongById from '@/hooks/useGetSongById';
+import useLoadSongUrl from '@/hooks/useLoadSongUrl';
+import usePlayer from '@/hooks/usePlayer';
+
+const Player = () => {
+    const player = usePlayer();
+    const { song } = useGetSongById(player.activeId);
+
+    const songUrl = useLoadSongUrl(song!);
+
+    if (!song || !songUrl || !player.activeId) {
+        return null;
+    }
+
+    return (
+        <div className="fixed bottom-0 h-20 w-full bg-black px-4 py-2">
+            Player
+        </div>
+    );
+};
+
+export default Player;
