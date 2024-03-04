d0bfc6dac022a2d9de165ee7871f02982d0129ad
yuchuncheng <yuchuncheng@nine-yi.com>
2024-03-04 11:52:12 +0800

    feat : add loading and error to project

---
+++ app/(site)/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Box from '@/components/box';
+
+const Error = () => {
+    return (
+        <Box className="flex h-full items-center justify-center">
+            <div>Something went wrong</div>
+        </Box>
+    );
+};
+
+export default Error;
